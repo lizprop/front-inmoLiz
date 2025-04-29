5be797aca5d76b3b5a942f6b20aea68c87054583
fmarcos38 <fmarcos_23@hotmail.com>
2025-04-28 21:51:28 -0300

    map

--- src/Components/MapaProp/index.jsx
+++ src/Components/MapaProp/index.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import './estilos.css'; // Archivo CSS para los estilos del mapa
 
-const MapProp = ({address}) => {
+const MapProp = ({ lat,lng }) => {
 
     //apikey google map
     const apiKey = process.env.REACT_APP_API_GOOGLE_MAP;
@@ -9,7 +9,7 @@ const MapProp = ({address}) => {
     // Función para generar la URL de Google Maps con la dirección proporcionada
     const generateMapUrl = (lat, lng) => {
         const baseUrl = `https://www.google.com/maps/embed/v1/place?key=${apiKey}`;
-        return `${baseUrl}&q=${encodeURIComponent(address)}`;
+        return `${baseUrl}&q=${lat},${lng}`;
     };
 
     return (
@@ -17,7 +17,7 @@ const MapProp = ({address}) => {
             <iframe
                 title="Map"
                 className="map"
-                src={generateMapUrl(address)}
+                src={generateMapUrl(lat, lng)}
                 allowFullScreen
             ></iframe>
         </div>

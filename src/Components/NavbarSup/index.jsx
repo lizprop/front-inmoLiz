88bae336fd3564127d720995a03de5994d348f8d
fmarcos38 <fmarcos_23@hotmail.com>
2026-04-26 20:47:27 -0300

    retoques

--- src/Components/NavbarSup/index.jsx
+++ src/Components/NavbarSup/index.jsx
@@ -15,7 +15,7 @@ function NavbarSup() {
                 <div className='cont-navbarSup__email'>
                     <EmailIcon sx={{ fontSize: 15, color: 'white' }} />
                     <a href="mailto: info@ortizlizmar" className='a-email-navSup'>
-                        <p className='p-tel-navSup'>ortizlizmar@gmail.com</p>
+                        <p className='p-tel-navSup'>ortizlizmar@hotmail.com</p>
                     </a>
                 </div>
             </div>
